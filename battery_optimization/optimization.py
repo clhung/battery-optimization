879c23aa614a281f0468c60b64a317806d3670d8
Chao-Ling Hung <chaoling.hung@gmail.com>
2025-03-23 16:23:11 -0400

    update optimization with dispatch

--- battery_optimization/optimization.py
+++ battery_optimization/optimization.py
@@ -31,13 +31,16 @@ class BatteryOptimization:
         self.battery = battery
         self.system = system
         self.ts = system.timesteps
+        self.dispatch_events = np.zeros(self.ts)
+        self.dispatch_amount = 1000
+        self.export_price = np.zeros(self.ts)
 
     def define_variables(self):
         self.charge = cp.Variable(self.ts, nonneg=True)
         self.discharge = cp.Variable(self.ts, nonneg=True)
         self.soc = cp.Variable(self.ts + 1, nonneg=True)
         self.solar_gen = cp.Variable(self.ts, nonneg=True)
-        self.grid_import = cp.Variable(self.ts, nonneg=True)
+        self.grid = cp.Variable(self.ts)
         # control charge and discharge
         self.z = cp.Variable(self.ts, boolean=True)
 
@@ -70,7 +73,7 @@ class BatteryOptimization:
 
             # power balance
             self.constraints += [
-                self.grid_import[t] + self.solar_gen[t] + self.discharge[t]
+                self.grid[t] + self.solar_gen[t] + self.discharge[t]
                 == self.system.load[t]
                 + self.charge[t] * self.battery.charge_efficiency
             ]
@@ -80,15 +83,40 @@ class BatteryOptimization:
                 self.charge[t] + self.solar_gen[t] <= self.system.solar[t]
             ]
 
-    def solve(self, initial_soc) -> pd.DataFrame:
+    def solve(
+        self, initial_soc: float, dispatch: bool = False
+    ) -> pd.DataFrame:
         self.define_variables()
         self.define_constraints(initial_soc=initial_soc)
-        obj = cp.Minimize(
-            cp.sum(cp.multiply(self.grid_import, self.system.price))
-        )
 
-        problem = cp.Problem(obj, self.constraints)
-        problem.solve(solver=cp.SCIPY, verbose=True)
+        timestamp = pd.to_datetime(self.system.timestamp)
+        if dispatch:
+            self.dispatch_events[
+                (timestamp.hour == 18) | (timestamp.hour == 19)
+            ] = 1
+
+        # Stage 1: Dispatch Target
+        dispatch_obj = cp.Maximize(
+            cp.sum(
+                cp.multiply(self.dispatch_events, self.discharge - self.grid)
+            )
+        )
+        problem1 = cp.Problem(dispatch_obj, self.constraints)
+        problem1.solve(solver=cp.SCIPY, verbose=True)
+
+        grid_fixed = self.grid.value.copy()
+
+        # Stage 2: Minimize Cost with Dispatch Results from Stage 1
+        fix_constraints = [
+            self.grid[t] == grid_fixed[t]
+            for t in range(self.ts)
+            if self.dispatch_events[t] == 1
+        ]
+        cost_obj = cp.Minimize(
+            cp.sum(cp.multiply(cp.pos(self.grid), self.system.price))
+        )
+        problem2 = cp.Problem(cost_obj, self.constraints + fix_constraints)
+        problem2.solve(solver=cp.SCIPY)
 
         return pd.DataFrame(
             data={
@@ -96,7 +124,7 @@ class BatteryOptimization:
                 "charge": self.charge.value,
                 "discharge": self.discharge.value,
                 "soc": self.soc.value[0:-1],
-                "grid_import": self.grid_import.value,
+                "grid": self.grid.value,
                 "solar_gen": self.solar_gen.value,
             }
         )
--- battery_optimization/scheduling_script.py
+++ battery_optimization/scheduling_script.py
@@ -6,11 +6,11 @@ from battery_optimization.optimization import (
 )
 
 
-def optimization_scheduler(start_time, optimization_horizon):
+def optimization_scheduler(start_time, optimization_horizon, dispatch):
     db_url = "postgresql://admin:admin@127.0.0.1:5432/db"
     ds = PostgresDataSource(db_url)
     data = ds.get_forecast_data(start_time, optimization_horizon)
-    battery = Battery(capacity=1250, max_charge=250, max_discharge=250)
+    battery = Battery(capacity=2600, max_charge=1000, max_discharge=1000)
     system = System(
         data["timestamp"].to_numpy(),
         data["solar"].to_numpy(),
@@ -20,8 +20,8 @@ def optimization_scheduler(start_time, optimization_horizon):
     optimizer = BatteryOptimization(battery, system)
     initial_soc = ds.get_initial_soc(start_time)
     if initial_soc is None:
-        initial_soc = 600
-    results_df = optimizer.solve(initial_soc=initial_soc)
+        initial_soc = 1300
+    results_df = optimizer.solve(initial_soc=initial_soc, dispatch=dispatch)
     ds.update_optimization_results(results_df)
 
     return data, results_df
@@ -30,4 +30,5 @@ def optimization_scheduler(start_time, optimization_horizon):
 if __name__ == "__main__":
     # for start_time in pd.date_range("2023-01-01", "2023-01-31", freq="1D"):
     #     optimization_scheduler(start_time, 36)
-    optimization_scheduler("2023-01-15", 36)
+    data, results = optimization_scheduler("2023-01-15", 36, dispatch=True)
+    print(results.head(5))
